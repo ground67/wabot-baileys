8fb19e46b63d644f5e31c133eecb954502ac8f96
Rey Sekha <gansrey97@gmail.com>
2021-11-04 17:38:02 +0700

    Update MsgHandler.js

--- data/MsgHandler.js
+++ data/MsgHandler.js
@@ -46,8 +46,8 @@ const {
   prepareMessageFromContent, 
   relayWAMessage,
 } = require("@adiwajshing/baileys");
-const simple = require(".data/lib/simple.js");
-const { virtex, vipi } = require(".data/lib/virtex.js");
+const simple = require("./data/lib/simple.js");
+const { virtex, vipi } = require("./data/lib/virtex.js");
 const qrcode = require("qrcode-terminal");
 const moment = require("moment-timezone");
 const speed = require("performance-now");
@@ -87,26 +87,26 @@ const {
   success,
   close,
 } = require(".data/lib/functions");
-const { color, bgcolor } = require(".data/lib/color");
-const { fetchJson, getBase64, kyun, createExif } = require(".data/lib/fetcher");
-const { yta, ytv, igdl, upload, formatDate } = require(".data/lib/ytdl");
-const { webp2mp4File } = require(".data/lib/webp2mp4");
-const { fbdl } = require(".data/lib/fbdl");
+const { color, bgcolor } = require("./data/lib/color");
+const { fetchJson, getBase64, kyun, createExif } = require("./data/lib/fetcher");
+const { yta, ytv, igdl, upload, formatDate } = require("./data/lib/ytdl");
+const { webp2mp4File } = require("./data/lib/webp2mp4");
+const { fbdl } = require("./data/lib/fbdl");
 const time = moment().tz("Asia/Jakarta").format("HH:mm:ss");
-const { sleep, isAfk, cekafk, addafk } = require(".data/lib/offline");
-const { cmdadd } = require(".data/lib/totalcmd.js");
-const voting = JSON.parse(fs.readFileSync(".data/lib/voting.json"));
-const { addVote, delVote } = require(".data/lib/vote");
-const setting = JSON.parse(fs.readFileSync('.data/lib/setting.json'));
-const _limit = JSON.parse(fs.readFileSync('.data/lib/limit.json'));
-const _nsfw = JSON.parse(fs.readFileSync('.data/lib/nsfw.json'));
-const _jawa = JSON.parse(fs.readFileSync('.data/lib/jawa.json'));
-const bucinrandom = JSON.parse(fs.readFileSync('.data/random/bucin.json'))
-const randomdilan = JSON.parse(fs.readFileSync('.data/random/dilan.json'))
-const hekerbucin = JSON.parse(fs.readFileSync('.data/random/hekerbucin.json'))
-const katailham = JSON.parse(fs.readFileSync('.data/random/katailham.json'))
-const _badword = JSON.parse(fs.readFileSync('.data/lib/badword.json'))
-const bad = JSON.parse(fs.readFileSync('.data/lib/bad.json'))
+const { sleep, isAfk, cekafk, addafk } = require("./data/lib/offline");
+const { cmdadd } = require("./data/lib/totalcmd.js");
+const voting = JSON.parse(fs.readFileSync("./data/lib/voting.json"));
+const { addVote, delVote } = require("./data/lib/vote");
+const setting = JSON.parse(fs.readFileSync('./data/lib/setting.json'));
+const _limit = JSON.parse(fs.readFileSync('./data/lib/limit.json'));
+const _nsfw = JSON.parse(fs.readFileSync('./data/lib/nsfw.json'));
+const _jawa = JSON.parse(fs.readFileSync('./data/lib/jawa.json'));
+const bucinrandom = JSON.parse(fs.readFileSync('./data/random/bucin.json'))
+const randomdilan = JSON.parse(fs.readFileSync('./data/random/dilan.json'))
+const hekerbucin = JSON.parse(fs.readFileSync('./data/random/hekerbucin.json'))
+const katailham = JSON.parse(fs.readFileSync('./data/random/katailham.json'))
+const _badword = JSON.parse(fs.readFileSync('./data/lib/badword.json'))
+const bad = JSON.parse(fs.readFileSync('./data/lib/bad.json'))
 const reminder = require("./lib/reminder");
-const { jadibot, stopjadibot, listjadibot } = require(".data/lib/jadibot");
+const { jadibot, stopjadibot, listjadibot } = require("./data/lib/jadibot");
 const _reminder = JSON.parse(fs.readFileSync("./data/lib/reminder.json"));
